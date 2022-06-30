cbc89ea520f88c6125d06aba3c44d70c6dff2df6
schaslivchik <schaslivchikm@gmail.com>
2022-06-30 12:50:02 +0300

    update

---
+++ js/slideshow.js
@@ -0,0 +1,10 @@
+const alertBanner = document.querySelector('.alert-banner');
+const focusBtnLeft = document.querySelector('.carousel-control-next')
+focusBtnLeft.focus()
+
+document.addEventListener("keydown", function (event) {
+  if (event.which === 122) {
+    focusBtnLeft.focus();
+    alertBanner.classList.add('active-banner');
+  }
+})
